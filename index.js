0fe2ee491cda3ebec35d6cb5f1180077d498c0c6
Alex Holhos <alex.holhos@emanuel.ro>
2018-02-12 18:40:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var happyHolidays = function(){
 var happyHolidaysTo = function(name){
   return "Happy holidays, "+name+"!";
 }
-var happyHolidayTo = function(name, holidays){
-  return "Happy " + holiday +name+"!";
+var happyHolidayTo = function(name, holiday){
+  return "Happy " + holiday + name + "!";
 }
